62eb1ec8eced49fa5843119593504a51791f2aa8
Hashma Sulthana Shaik <160395716+Hashmasulthana@users.noreply.github.com>
2026-04-07 20:52:27 +0530

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -1,28 +1,24 @@
-const sqlite3 = require('sqlite3').verbose();
+const Database = require('better-sqlite3');
 
-// Create DB file
-const db = new sqlite3.Database('./inventory.db');
+// Create DB
+const db = new Database('inventory.db');
 
 // Create tables
-db.serialize(() => {
-  db.run(`
-    CREATE TABLE IF NOT EXISTS suppliers (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT NOT NULL,
-      city TEXT NOT NULL
-    )
-  `);
+db.exec(`
+  CREATE TABLE IF NOT EXISTS suppliers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    city TEXT NOT NULL
+  );
 
-  db.run(`
-    CREATE TABLE IF NOT EXISTS inventory (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      supplier_id INTEGER,
-      product_name TEXT,
-      quantity INTEGER,
-      price REAL,
-      FOREIGN KEY (supplier_id) REFERENCES suppliers(id)
-    )
-  `);
-});
+  CREATE TABLE IF NOT EXISTS inventory (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    supplier_id INTEGER,
+    product_name TEXT,
+    quantity INTEGER,
+    price REAL,
+    FOREIGN KEY (supplier_id) REFERENCES suppliers(id)
+  );
+`);
 
 module.exports = db;
